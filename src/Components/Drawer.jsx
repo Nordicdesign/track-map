c28dcc011363fcd17f8d9e9b711cb18a43cd0385
Alex Lillo <alex.lillo@gmail.com>
2019-04-18 15:17:05 +0200

    toggle buttons work

--- src/Components/Drawer.jsx
+++ src/Components/Drawer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CornerArea from '../Components/CornerArea';
 
 class Drawer extends Component {
 
@@ -16,45 +17,11 @@ class Drawer extends Component {
         <button onClick={this.props.onClick}>Close</button>
         <h2>Turn {this.props.corner}</h2>
 
-        <p>Entry</p>
-        <ul data-area="entry">
-          <li id="entry-under">
-            <button type="button" value="under"
-              className={(this.props.corner === 'understeer') ? "selected" : null}
-            >
-              <strong>UNDER</strong>steer
-            </button>
-          </li>
-          <li id="entry-over">
-            <button
-              type="button"
-              value="over"
-              className={this.props.selected ? "selected" : null}
-            >
-              <strong>OVER</strong>steer
-            </button>
-          </li>
-        </ul>
+        <CornerArea area="Entry" />
+        <CornerArea area="Mid-corner" />
+        <CornerArea area="Exit" />
 
-        <p>Mid corner</p>
 
-        <p>Exit</p>
-        <ul data-area="exit">
-          <li id="exit-under">
-            <button
-              type="button"
-              value="under"
-            >
-              <strong>UNDER</strong>steer</button>
-          </li>
-          <li id="exit-over">
-            <button
-              type="button"
-              value="over"
-            >
-              <strong>OVER</strong>steer</button>
-          </li>
-        </ul>
 
         <h3>Breaking reference point</h3>
         <input type="textarea" value="" name="breakingPoint" id="breakingPoint"/>
