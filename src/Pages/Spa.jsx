fb241e45613eee5229354d1395b90d1b7aaa8585
Alex Lillo <alex.lillo@gmail.com>
2019-04-18 10:18:10 +0200

    all corners open the drawer

--- src/Pages/Spa.jsx
+++ src/Pages/Spa.jsx
@@ -10,7 +10,7 @@ class Spa extends Component {
       isOpen: false,
       corner: null
     };
-    
+
     this.toggleDrawer = this.toggleDrawer.bind(this);
   }
 
@@ -42,28 +42,28 @@ class Spa extends Component {
       <button onClick={this.toggleDrawer(1)}>Open turn 1</button>
 
       <div className="track">
-        <img src="/images/Spa-Francorchamps_of_Belgium.svg" useMap="#image-map" alt="Map of Spa Francorchamps circuit"/>
+        <img id="imageMap" src="/images/Spa-Francorchamps_of_Belgium.svg" useMap="#image-map" alt="Map of Spa Francorchamps circuit"/>
         <map name="image-map">
-          <area target="" alt="2" title="2" href="" coords="65,571,40" shape="circle"/>
-          <area target="" alt="1" title="1" href="" coords="292,803,36" shape="circle"/>
-          <area target="" alt="3" title="3" href="" coords="181,366,32" shape="circle"/>
-          <area target="" alt="4" title="4" href="" coords="218,297,34" shape="circle"/>
-          <area target="" alt="5" title="5" href="" coords="313,264,34" shape="circle"/>
-          <area target="" alt="6" title="6" href="" coords="545,173,36" shape="circle"/>
-          <area target="" alt="7" title="7" href="" coords="1076,68,33" shape="circle"/>
-          <area target="" alt="8" title="8" href="" coords="1140,101,35" shape="circle"/>
-          <area target="" alt="9" title="9" href="" coords="1224,76,36" shape="circle"/>
-          <area target="" alt="10" title="10" href="" coords="1389,116,37" shape="circle"/>
-          <area target="" alt="11" title="11" href="" coords="1247,200,39" shape="circle"/>
-          <area target="" alt="12" title="12" href="" coords="853,348,36" shape="circle"/>
-          <area target="" alt="13" title="13" href="" coords="1245,515,37" shape="circle"/>
-          <area target="" alt="14" title="14" href="" coords="1162,625,39" shape="circle"/>
-          <area target="" alt="15" title="15" href="" coords="1353,676,35" shape="circle"/>
-          <area target="" alt="16" title="16" href="" coords="1257,820,38" shape="circle"/>
-          <area target="" alt="17" title="17" href="" coords="956,561,37" shape="circle"/>
-          <area target="" alt="18" title="18" href="" coords="710,473,36" shape="circle"/>
-          <area target="" alt="19" title="19" href="" coords="346,678,38" shape="circle"/>
-          <area target="" alt="20" title="20" href="" coords="385,538,40" shape="circle"/>
+          <area target="" alt="2" title="2" href="#" coords="43,379,27" shape="circle" onClick={this.toggleDrawer(2)} />
+          <area target="" alt="1" title="1" href="#" coords="194,533,24" shape="circle" onClick={this.toggleDrawer(1)} />
+          <area target="" alt="3" title="3" href="#" coords="120,243,21" shape="circle" onClick={this.toggleDrawer(3)} />
+          <area target="" alt="4" title="4" href="#" coords="145,197,23" shape="circle" onClick={this.toggleDrawer(4)} />
+          <area target="" alt="5" title="5" href="#" coords="208,175,23" shape="circle" onClick={this.toggleDrawer(5)} />
+          <area target="" alt="6" title="6" href="#" coords="362,115,24" shape="circle" onClick={this.toggleDrawer(6)} />
+          <area target="" alt="7" title="7" href="#" coords="714,45,22" shape="circle" onClick={this.toggleDrawer(7)} />
+          <area target="" alt="8" title="8" href="#" coords="757,67,23" shape="circle" onClick={this.toggleDrawer(8)} />
+          <area target="" alt="9" title="9" href="#" coords="813,50,24" shape="circle" onClick={this.toggleDrawer(9)} />
+          <area target="" alt="10" title="10" href="#" coords="922,77,25" shape="circle" onClick={this.toggleDrawer(10)} />
+          <area target="" alt="11" title="11" href="#" coords="828,133,26" shape="circle" onClick={this.toggleDrawer(11)} />
+          <area target="" alt="12" title="12" href="#" coords="566,231,24" shape="circle" onClick={this.toggleDrawer(12)} />
+          <area target="" alt="13" title="13" href="#" coords="826,342,25" shape="circle" onClick={this.toggleDrawer(13)} />
+          <area target="" alt="14" title="14" href="#" coords="771,415,26" shape="circle" onClick={this.toggleDrawer(14)} />
+          <area target="" alt="15" title="15" href="#" coords="898,449,23" shape="circle" onClick={this.toggleDrawer(15)} />
+          <area target="" alt="16" title="16" href="#" coords="834,544,25" shape="circle" onClick={this.toggleDrawer(16)} />
+          <area target="" alt="17" title="17" href="#" coords="635,372,25" shape="circle" onClick={this.toggleDrawer(17)} />
+          <area target="" alt="18" title="18" href="#" coords="471,314,24" shape="circle" onClick={this.toggleDrawer(18)} />
+          <area target="" alt="19" title="19" href="#" coords="230,450,25" shape="circle" onClick={this.toggleDrawer(19)} />
+          <area target="" alt="20" title="20" href="#" coords="256,357,27" shape="circle"  onClick={this.toggleDrawer(20)} />
         </map>
       </div>
       <Drawer
