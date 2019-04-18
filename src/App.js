23026d5d62456ab9ae9b1b0f0326280a433deb7f
Alex Lillo <alex.lillo@gmail.com>
2019-04-18 10:17:41 +0200

    using sass instead of css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
-import './App.css';
+import './Scss/App.scss';
 
 import Homepage from './Pages/Homepage'
 import Spa from './Pages/Spa'
