4dd5393db165a8a6f4d9491faf653727899b6817
alex.lillo@gmail.com <alex.lillo@gmail.com>
2019-06-07 10:06:04 +0100

    added header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import './App.scss';
 
+import Header from './Components/Header'
 import Homepage from './Pages/Homepage'
 import Spa from './Pages/Spa'
 import Fb from './Pages/Fb'
@@ -12,14 +13,18 @@ import Fb from './Pages/Fb'
 class App extends Component {
   render() {
     return (
-      <Router>
-      <div className="App">
-         <Route exact path="/" component={Homepage}/>
-         <Route path="/spa" component={Spa}/>
-         <Route path="/fb" component={Fb}/>
+      <div>
+        <Header />
+        <Router>
+          <div className="App">
 
-      </div>
-    </Router>
+             <Route exact path="/" component={Homepage}/>
+             <Route path="/spa" component={Spa}/>
+             <Route path="/fb" component={Fb}/>
+
+          </div>
+      </Router>
+    </div>
     );
   }
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <header>
+        <p><a href="/">TrackMap</a></p>
+      </header>
+    )
+  }
+}
+
+export default Header;
