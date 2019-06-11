d68f2d729baacd1a1e6fd09e751e587bc556653a
alex.lillo@gmail.com <alex.lillo@gmail.com>
2019-06-11 08:55:52 +0100

    renamed Fb to Spa

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import './App.scss';
 import Header from './Components/Header'
 import Homepage from './Pages/Homepage'
 import Spa from './Pages/Spa'
-import Fb from './Pages/Fb'
 
 class App extends Component {
   render() {
@@ -20,7 +19,6 @@ class App extends Component {
 
              <Route exact path="/" component={Homepage}/>
              <Route path="/spa" component={Spa}/>
-             <Route path="/fb" component={Fb}/>
 
           </div>
       </Router>
--- src/Pages/Spa.jsx
+++ src/Pages/Spa.jsx
@@ -1,5 +1,22 @@
-import React, {Component} from 'react';
-import Drawer from '../Components/Drawer';
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import update from 'immutability-helper';
+
+var firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: "https://trackmap-f1119.firebaseio.com",
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: "",
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 class Spa extends Component {
 
@@ -7,72 +24,162 @@ class Spa extends Component {
     super(props,context);
 
     this.state = {
-      isOpen: false,
-      corner: null
+      trackName: "Spa Francorchamps",
+      turns: [
+        ,
+        {},
+        {},
+        {},
+        {}
+      ]
     };
 
-    this.toggleDrawer = this.toggleDrawer.bind(this);
+    this.updateTurn = this.updateTurn.bind(this);
+    this.registerNotes = this.registerNotes.bind(this);
   }
 
-  toggleDrawer = (corner) => () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-      corner: corner,
-    });
-  };
+  loadData() {
+    let that = this; //🤯
+    firebase.database().ref('/users/0/tracks/0/turn').on('value', function(snapshot) {
+      let turns = snapshot.val()
+      console.log(turns);
 
-  // setSelected = (corner, type, behaviour) => () => {
-  //   // corner: numeric
-  //   // type: entry, mid, exit
-  //   // behaviour: oversteer, understeer
-  //   this.setState({
-  //     behaviour: {
-  //       [corner]: {
-  //         [type]: behaviour
-  //       }
-  //     }
-  //
-  //   });
-  //   console.log(this.state.behaviour);
-  // };
+      that.setState({
+        turns: update(that.state.turns, {$merge: turns})
+      })
+    });
+  }
+
+  registerNotes(event, turnID) {
+    console.log(event.target.value);
+    var updates = {};
+    updates['/users/0/tracks/0/turn/' + turnID + '/notes'] = event.target.value;
+
+    return firebase.database().ref().update(updates);
+
+  }
+
+  updateTurn(turnID, section, behaviour) {
+
+    console.log(turnID);
+
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    var updates = {};
+    updates['/users/0/tracks/0/turn/' + turnID + '/' + section] = behaviour;
+
+    return firebase.database().ref().update(updates);
+  }
+
+  componentDidMount() {
+    var updates = {};
+    updates['/users/0/tracks/0/name'] = "Spa Francorchamps";
+
+    firebase.database().ref().update(updates);
+    this.loadData();
+  }
 
   render() {
-    return (<div>
-      <h1>Spa Franchorchamps</h1>
-      <button onClick={this.toggleDrawer(1)}>Open turn 1</button>
+    console.log(this.state.turns);
 
-      <div className="track">
-        <img id="imageMap" src="/images/Spa-Francorchamps_of_Belgium.svg" useMap="#image-map" alt="Map of Spa Francorchamps circuit"/>
-        <map name="image-map">
-          <area target="" alt="2" title="2" href="#" coords="43,379,27" shape="circle" onClick={this.toggleDrawer(2)} />
-          <area target="" alt="1" title="1" href="#" coords="194,533,24" shape="circle" onClick={this.toggleDrawer(1)} />
-          <area target="" alt="3" title="3" href="#" coords="120,243,21" shape="circle" onClick={this.toggleDrawer(3)} />
-          <area target="" alt="4" title="4" href="#" coords="145,197,23" shape="circle" onClick={this.toggleDrawer(4)} />
-          <area target="" alt="5" title="5" href="#" coords="208,175,23" shape="circle" onClick={this.toggleDrawer(5)} />
-          <area target="" alt="6" title="6" href="#" coords="362,115,24" shape="circle" onClick={this.toggleDrawer(6)} />
-          <area target="" alt="7" title="7" href="#" coords="714,45,22" shape="circle" onClick={this.toggleDrawer(7)} />
-          <area target="" alt="8" title="8" href="#" coords="757,67,23" shape="circle" onClick={this.toggleDrawer(8)} />
-          <area target="" alt="9" title="9" href="#" coords="813,50,24" shape="circle" onClick={this.toggleDrawer(9)} />
-          <area target="" alt="10" title="10" href="#" coords="922,77,25" shape="circle" onClick={this.toggleDrawer(10)} />
-          <area target="" alt="11" title="11" href="#" coords="828,133,26" shape="circle" onClick={this.toggleDrawer(11)} />
-          <area target="" alt="12" title="12" href="#" coords="566,231,24" shape="circle" onClick={this.toggleDrawer(12)} />
-          <area target="" alt="13" title="13" href="#" coords="826,342,25" shape="circle" onClick={this.toggleDrawer(13)} />
-          <area target="" alt="14" title="14" href="#" coords="771,415,26" shape="circle" onClick={this.toggleDrawer(14)} />
-          <area target="" alt="15" title="15" href="#" coords="898,449,23" shape="circle" onClick={this.toggleDrawer(15)} />
-          <area target="" alt="16" title="16" href="#" coords="834,544,25" shape="circle" onClick={this.toggleDrawer(16)} />
-          <area target="" alt="17" title="17" href="#" coords="635,372,25" shape="circle" onClick={this.toggleDrawer(17)} />
-          <area target="" alt="18" title="18" href="#" coords="471,314,24" shape="circle" onClick={this.toggleDrawer(18)} />
-          <area target="" alt="19" title="19" href="#" coords="230,450,25" shape="circle" onClick={this.toggleDrawer(19)} />
-          <area target="" alt="20" title="20" href="#" coords="256,357,27" shape="circle"  onClick={this.toggleDrawer(20)} />
-        </map>
+    return (
+        <div className="wrapper">
+          <div className="track">
+            <div className="track-logs">
+              <h1>{this.state.trackName}</h1>
+
+              <ul className="corner-areas">
+                <li>Entry</li>
+                <li>Mid</li>
+                <li>Exit</li>
+              </ul>
+
+              {this.state.turns.map((turn,i) =>
+                  <ul className="track-turn">
+                    <li className="track-turn-individual">
+                      <div>
+                        <p>{i}</p>
+
+                        <ul>
+                          <li key={'entry' + i}>
+                            <span>Entry: {turn.entry ? turn.entry : 'neutral'}</span>
+                            <button
+                              onClick={() => this.updateTurn(i,"entry","understeer")}
+                              >
+                              U
+                            </button>
+                            <button
+                              onClick={() => this.updateTurn(i,"entry","neutral")}
+                              >
+                              N
+                            </button>
+                            <button
+                              onClick={() => this.updateTurn(i,"entry","oversteer")}
+                              >
+                              O
+                            </button>
+                          </li>
+                          <li key={'mid' + i}>
+                            <span>Mid: {turn.mid ? turn.mid : 'neutral'}</span>
+                            <button
+                              onClick={() => this.updateTurn(i,"mid","understeer")}
+                              >
+                              U
+                            </button>
+                            <button
+                              onClick={() => this.updateTurn(i,"mid","neutral")}
+                              >
+                              N
+                            </button>
+                            <button
+                              onClick={() => this.updateTurn(i,"mid","oversteer")}
+                              >
+                              O
+                            </button>
+                          </li>
+                          <li key={'exit' + i}>
+                            <span>Entry: {turn.exit ? turn.exit : 'neutral'}</span>
+                            <button
+                              onClick={() => this.updateTurn(i,"exit","understeer")}
+                              >
+                              U
+                            </button>
+                            <button
+                              onClick={() => this.updateTurn(i,"exit","neutral")}
+                              >
+                              N
+                            </button>
+                            <button
+                              onClick={() => this.updateTurn(i,"exit","oversteer")}
+                              >
+                              O
+                            </button>
+                          </li>
+
+                        </ul>
+                      </div>
+
+                      <p className="track-turn-notes">
+                        <label>
+                          Notes
+                          <textarea
+                            name=""
+                            id=""
+                            value={turn.notes ? turn.notes : ""}
+                            onChange={(e) => this.registerNotes(e, i)}
+                           />
+                        </label>
+                      </p>
+                    </li>
+                </ul>
+              )};
+          </div>
+
+          <div className="track-map">
+            <img src="/images/Spa-Francorchamps_of_Belgium.svg" alt="Map of Spa Francorchamps circuit"/>
+          </div>
+        </div>
       </div>
-      <Drawer
-        isOpen={this.state.isOpen}
-        onClick={this.toggleDrawer(this.state.corner)}
-        corner={this.state.corner}
-      />
-    </div>)
-
+    );
   }
 }
 
