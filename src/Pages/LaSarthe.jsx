b33a2e4de4969309203f19d777b4972e3404f1c2
Alex Lillo <alex.lillo@gmail.com>
2019-06-18 23:42:13 +0100

    Need to be signed in to view the pages

--- src/Pages/LaSarthe.jsx
+++ src/Pages/LaSarthe.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
-// Firebase App (the core Firebase SDK) is always required and must be listed first
 import * as firebase from "firebase/app";
-// import "firebase/auth";
 import "firebase/database";
 import Firebase from "../Components/Firebase"
 import update from 'immutability-helper';
 import TrackLogs from '../Components/TrackLogs'
+import {NotLoggedIn} from '../Components/NotLoggedIn'
 
 class LaSarthe extends Component {
 
   constructor(props,context) {
     super(props,context);
-
     this.state = {
       trackName: "Circuit de La Sarthe",
+      authUser: null,
+      userEmail: null,
+      error: null,
       turns: [
         ,
         {},
@@ -45,7 +46,6 @@ class LaSarthe extends Component {
         {}
       ]
     };
-
     this.updateTurn = this.updateTurn.bind(this);
     this.registerNotes = this.registerNotes.bind(this);
   }
@@ -66,33 +66,44 @@ class LaSarthe extends Component {
     console.log(event.target.value);
     var updates = {};
     updates['/users/0/tracks/1/turn/' + turnID + '/notes'] = event.target.value;
-
     return firebase.database().ref().update(updates);
-
   }
 
   updateTurn(turnID, section, behaviour) {
-
-    console.log(turnID);
-
     // Write the new post's data simultaneously in the posts list and the user's post list.
     var updates = {};
     updates['/users/0/tracks/1/turn/' + turnID + '/' + section] = behaviour;
-
     return firebase.database().ref().update(updates);
   }
 
   componentDidMount() {
+    // check the user
+    let that = this;
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        var userEmail = user.email;
+        var uid = user.uid;
+        that.setState({
+          authUser: uid,
+          userEmail: userEmail
+        })
+      } else {
+        that.setState({
+          authUser: null,
+          userEmail: null,
+          error: null
+        })
+      }
+    });
+    // do something about the track
     var updates = {};
     updates['/users/0/tracks/1/name'] = "Circuit de la Sarthe";
-
     firebase.database().ref().update(updates);
     this.loadData();
   }
 
   render() {
-    // console.log(this.state.turns);
-
     const trackTurns = [
       "",
       "Dunlop curve",
@@ -124,19 +135,21 @@ class LaSarthe extends Component {
 
     return (
       <div className="wrapper">
-        <div className="track">
-          <TrackLogs
-            turns={this.state.turns}
-            trackName={this.state.trackName}
-            updateTurn={this.updateTurn}
-            registerNotes={this.registerNotes}
-            turnNames={trackTurns}
-          />
+        {!this.state.authUser ? <NotLoggedIn/> : 
+          <div className="track">
+            <TrackLogs
+              turns={this.state.turns}
+              trackName={this.state.trackName}
+              updateTurn={this.updateTurn}
+              registerNotes={this.registerNotes}
+              turnNames={trackTurns}
+            />
 
-          <div className="track-map">
-            <img src="/images/la-sarthe.png" alt="Map of la Sarthe circuit"/>
+            <div className="track-map">
+              <img src="/images/la-sarthe.png" alt="Map of la Sarthe circuit"/>
+            </div>
           </div>
-        </div>
+        }
       </div>
     );
   }
--- src/Pages/Spa.jsx
+++ src/Pages/Spa.jsx
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/database";
 import Firebase from "../Components/Firebase"
-import update from 'immutability-helper';
+import * as firebase from 'firebase/app'
+import "firebase/database"
+import update from 'immutability-helper'
 import TrackLogs from '../Components/TrackLogs'
+import {NotLoggedIn} from '../Components/NotLoggedIn'
 
 class Spa extends Component {
-
   constructor(props,context) {
     super(props,context);
-
     this.state = {
       trackName: "Spa Francorchamps",
+      authUser: null,
+      userEmail: null,
+      error: null,
       turns: [
         ,
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
         {},
         {},
         {},
         {}
       ]
     };
-
     this.updateTurn = this.updateTurn.bind(this);
     this.registerNotes = this.registerNotes.bind(this);
   }
@@ -33,7 +48,6 @@ class Spa extends Component {
     firebase.database().ref('/users/0/tracks/0/turn').on('value', function(snapshot) {
       let turns = snapshot.val()
       console.log(turns);
-
       that.setState({
         turns: update(that.state.turns, {$merge: turns})
       })
@@ -44,42 +58,54 @@ class Spa extends Component {
     console.log(event.target.value);
     var updates = {};
     updates['/users/0/tracks/0/turn/' + turnID + '/notes'] = event.target.value;
-
     return firebase.database().ref().update(updates);
-
   }
 
   updateTurn(turnID, section, behaviour) {
-
     console.log(turnID);
-
     // Write the new post's data simultaneously in the posts list and the user's post list.
     var updates = {};
     updates['/users/0/tracks/0/turn/' + turnID + '/' + section] = behaviour;
-
     return firebase.database().ref().update(updates);
   }
 
   componentDidMount() {
+    let that = this;
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        var userEmail = user.email;
+        var uid = user.uid;
+        that.setState({
+          authUser: uid,
+          userEmail: userEmail
+        })
+      } else {
+        that.setState({
+          authUser: null,
+          userEmail: null,
+          error: null
+        })
+      }
+    });
     var updates = {};
     updates['/users/0/tracks/0/name'] = "Spa Francorchamps";
-
     firebase.database().ref().update(updates);
     this.loadData();
   }
 
   render() {
-
-    const trackTurns = [ ];
-
+    const trackTurns = ["La Source", "", "", "", "Raidillon", "Eau Rouge", "", "Les Combes" ];
     return (
       <div className="wrapper">
+      {!this.state.authUser ? <NotLoggedIn/> :
+
         <div className="track">
           <TrackLogs
             turns={this.state.turns}
             trackName={this.state.trackName}
-            updateTurn={this.updateTurn}
-            registerNotes={this.registerNotes}
+            updateTurn={this.state.updateTurn}
+            registerNotes={this.state.registerNotes}
             turnNames={trackTurns}
           />
 
@@ -87,6 +113,8 @@ class Spa extends Component {
             <img src="/images/Spa-Francorchamps_of_Belgium.svg" alt="Map of Spa Francorchamps circuit"/>
           </div>
         </div>
+
+      }
       </div>
     );
   }
