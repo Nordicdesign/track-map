962410e92b7eb7bc07623e23819c3d27ccb67658
Alex Lillo <alex.lillo@gmail.com>
2019-08-23 12:42:54 +0200

    fix

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
 import * as firebase from "firebase/app"
 import "firebase/auth"
 import * as ROUTES from '../constants/routes'
