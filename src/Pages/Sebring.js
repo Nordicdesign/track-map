778f011ac5bb6431fde1304f00ef4c5ce83d13e7
Alex Lillo <alex.lillo@gmail.com>
2019-10-10 23:07:23 +0100

    added image map for Sebring

--- src/Pages/Sebring.js
+++ src/Pages/Sebring.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Track from '../Components/Track'
 
 const numberTurns = 17 + 1; // Add the number of actual turns, but we need an extra one for the zero
-const imgWidth = 1580;
+const imgWidth = 990;
 const canvasMargin = 500;
 const trackName= "Sebring";
 const trackID= "4";
@@ -10,25 +10,26 @@ const URL = "/images/Sebring_International_Raceway.svg";
 const MAP = {
   name: "my-map",
   areas: [
-    { name: "1", shape: "circle", coords: [551,20,15], fillColor: "rgba(0, 0, 0, 0.25)"  },
-    { name: "2", shape: "circle", coords: [607,119,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "3", shape: "circle", coords: [560,74,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "4", shape: "circle", coords: [492,129,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "5", shape: "circle", coords: [676,174,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "6", shape: "circle", coords: [767,285,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "7", shape: "circle", coords: [558,239,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "8", shape: "circle", coords: [500,370,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "9", shape: "circle", coords: [382,292,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "10", shape: "circle", coords: [345,368,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "11", shape: "circle", coords: [557,477,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "12", shape: "circle", coords: [606,457,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "13", shape: "circle", coords: [633,575,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "14", shape: "circle", coords: [23,250,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "15", shape: "circle", coords: [68,182,15], fillColor: "rgba(0, 0, 0, 0.25)" },
-    { name: "16", shape: "circle", coords: [196,261,15], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "1", shape: "circle", coords: [745,549,22], fillColor: "rgba(0, 0, 0, 0.25)"  },
+    { name: "2", shape: "circle", coords: [723,357,21], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "3", shape: "circle", coords: [724,267,21], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "4", shape: "circle", coords: [706,198,26], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "5", shape: "circle", coords: [628,230,21], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "6", shape: "circle", coords: [490,313,22], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "7", shape: "circle", coords: [26,367,21], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "8", shape: "circle", coords: [154,184,22], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "9", shape: "circle", coords: [296,136,23], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "10", shape: "circle", coords: [477,51,26], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "11", shape: "circle", coords: [601,41,25], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "12", shape: "circle", coords: [714,72,24], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "13", shape: "circle", coords: [795,74,26], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "14", shape: "circle", coords: [868,371,25], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "15", shape: "circle", coords: [929,580,25], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "16", shape: "circle", coords: [881,717,24], fillColor: "rgba(0, 0, 0, 0.25)" },
+    { name: "16", shape: "circle", coords: [65,688,24], fillColor: "rgba(0, 0, 0, 0.25)" },
   ]
 }
-
+ 
 export default function Sebring() {
   return (
     <Track
