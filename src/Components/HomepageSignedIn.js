ffeac768c3813afb41650ca8acab2bf836c1a8f8
Alex Lillo <alex.lillo@gmail.com>
2020-07-13 11:21:17 +0100

    Organised by continent

--- src/Components/HomepageSignedIn.js
+++ src/Components/HomepageSignedIn.js
@@ -7,17 +7,28 @@ export const HomepageSignedIn = () => {
       <div className="homepage">
         <main>
           <h1>Select a track</h1>
+          <h2>North America</h2>
           <ul className="listCircuits">
-            <li><Link to={ROUTES.BARCELONA}>Circuit de Catalunya (Barcelona)</Link></li>
             <li><Link to={ROUTES.DAYTONA}>Daytona - Road course</Link></li>
-            <li><Link to={ROUTES.LEMANS}>Le Mans - Circuit de la Sarthe</Link></li>
-            <li><Link to={ROUTES.BATHURST}>Mount Panorama - Bathurst</Link></li>
             <li><Link to={ROUTES.ROADAMERICA}>Road America</Link></li>
             <li><Link to={ROUTES.SEBRING}>Sebring</Link></li>
-            <li><Link to={ROUTES.SHANGHAI}>Shanghai</Link></li>
-            <li><Link to={ROUTES.SPA}>Spa-Franchorchamps</Link></li>
             <li><Link to={ROUTES.WATKINS}>Watkins Glen</Link></li>
+          </ul>
+          <h2>Europe</h2>
+          <ul className="listCircuits">
+            <li><Link to={ROUTES.BARCELONA}>Circuit de Catalunya (Barcelona)</Link></li>
+            <li><Link to={ROUTES.LEMANS}>Le Mans - Circuit de la Sarthe</Link></li>
+            <li><Link to={ROUTES.SILVERSTONE}>Silverstone</Link></li>
+            <li><Link to={ROUTES.SPA}>Spa-Franchorchamps</Link></li>
             <li><Link to={ROUTES.ZOLDER}>Zolder</Link></li>
+          </ul>
+          <h2>Asia</h2>
+          <ul className="listCircuits">
+            <li><Link to={ROUTES.SHANGHAI}>Shanghai</Link></li>
+          </ul>
+          <h2>Australia</h2>
+          <ul className="listCircuits">
+            <li><Link to={ROUTES.BATHURST}>Mount Panorama - Bathurst</Link></li>
           </ul>
         </main>
         <aside>
