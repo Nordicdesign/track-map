94615a4104d7034784056a58b9486b5b4b04322c
Alex Lillo <alex.lillo@gmail.com>
2024-01-21 10:30:26 +0000

    Fix notes issues (#57)
    * nvm set to 18.19
    
    * lint fixes
    
    * remove comment
    
    * add types
    
    * makes link buttons visible
    
    * slightly better buttons
    
    * fix: make vercel routes work

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
   env: {
     es6: true,
     browser: true,
@@ -19,7 +19,20 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
+    'no-console': 'warn',
     '@typescript-eslint/no-explicit-any': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
   },
 }
